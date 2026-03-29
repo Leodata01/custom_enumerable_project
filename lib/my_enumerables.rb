883e25e2a3d01174abbbab5026ab6fb3adbb780b
leo <187560246+Leodata01@users.noreply.github.com>
2026-03-29 12:14:59 +0700

    finish exercice

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,104 @@
 module Enumerable
-  # Your code goes here
+  def my_all?
+    my_each do |n|
+      return false unless yield(n)
+    end
+
+    true
+  end
+
+  def my_any?
+    each do |n|
+      return true if yield(n)
+    end
+
+    false
+  end
+
+  def my_count(arg = nil)
+    return size if arg.nil? && !block_given?
+
+    count = 0
+    my_each do |n|
+      if arg.nil?
+        count += 1 if yield(n)
+      elsif n == arg
+        count += 1
+      end
+    end
+
+    count
+  end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    index = 0
+    my_each do |item|
+      yield(item, index)
+      index += 1
+    end
+
+    self
+  end
+
+  def my_inject(initial_value = nil)
+    total = initial_value
+
+    no_initial_value_provided = initial_value.nil?
+
+    my_each do |n|
+      if no_initial_value_provided
+        total = n
+        no_initial_value_provided = false
+      else
+        total = yield(total, n)
+      end
+    end
+
+    total
+  end
+
+  def my_map
+    return to_enum(:my_map) unless block_given?
+
+    new_arr = []
+    my_each do |n|
+      new_arr << yield(n)
+    end
+
+    new_arr
+  end
+
+  def my_none?
+    none = true
+    my_each do |n|
+      none = false if yield(n)
+    end
+
+    none
+  end
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    selected = []
+    my_each do |n|
+      selected << n if yield(n)
+    end
+
+    selected
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    for i in self do
+      yield(i)
+    end
+
+    self
+  end
 end
